7bbb87426d073aa79d8bb27ed796ee2f519b37b3
Bas van der Hoek <bas@hoek.com>
2025-02-11 16:23:39 +0100

    Add env_logging at debug

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use rustpiboot::{boot, Options};
 
 fn main() {
+    env_logger::Builder::from_default_env()
+      .filter_level(log::LevelFilter::Debug)
+      .init();
     boot(Options::default()).unwrap();
 }
